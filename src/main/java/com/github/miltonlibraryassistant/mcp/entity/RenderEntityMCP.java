5944bf20c4097e044850fd6d7d60ce775f4b278e
MiltonLibraryAssistant <dahlia.hawthorne42@gmail.com>
2015-10-18 20:51:42 -0700

    More adjustments to initial framework

--- src/main/java/com/github/miltonlibraryassistant/mcp/entity/RenderEntityMCP.java
+++ src/main/java/com/github/miltonlibraryassistant/mcp/entity/RenderEntityMCP.java
@@ -1,6 +1,7 @@
 package com.github.miltonlibraryassistant.mcp.entity;
 
 import com.github.miltonlibraryassistant.mcp.References;
+import com.github.miltonlibraryassistant.mcp.models.blockmodel;
 
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
@@ -10,11 +11,12 @@ import net.minecraft.util.ResourceLocation;
 public class RenderEntityMCP extends RenderLiving {
 	
 	private static final ResourceLocation mcpTexture = new ResourceLocation(References.modId + ":textures/entity/EntityCityBird.png");
-    private neoncitybirdmodel mcpModel;
+    private blockmodel mcpModel; 
+    //It's a block. Not even a good block. It floats. Laziest Techne model ever. Srsly if one of you guys wants to make a better model please do
     
     public RenderEntityMCP(ModelBase par1ModelBase, float par2) {
 		super(par1ModelBase, par2);
-        this.mcpModel = (neoncitybirdmodel)super.mainModel;
+        this.mcpModel = (blockmodel)super.mainModel;
         this.setRenderPassModel(this.mcpModel);
 	}
 
--- src/main/java/com/github/miltonlibraryassistant/mcp/proxy/ProxyClient.java
+++ src/main/java/com/github/miltonlibraryassistant/mcp/proxy/ProxyClient.java
@@ -2,6 +2,7 @@ package com.github.miltonlibraryassistant.mcp.proxy;
 
 import com.github.miltonlibraryassistant.mcp.entity.EntityMCP;
 import com.github.miltonlibraryassistant.mcp.entity.RenderEntityMCP;
+import com.github.miltonlibraryassistant.mcp.models.blockmodel;
 
 import cpw.mods.fml.client.registry.RenderingRegistry;
 
@@ -11,7 +12,7 @@ public class ProxyClient extends ProxyCommon {
 		}
 		
 		public void registerRenderThings(){
-			RenderingRegistry.registerEntityRenderingHandler(EntityMCP.class, new RenderEntityMCP(new neoncitybirdmodel(), 1));
+			RenderingRegistry.registerEntityRenderingHandler(EntityMCP.class, new RenderEntityMCP(new blockmodel(), 1));
 		}
 		
 		public void registerTileEntitySpecialRenderer(){
